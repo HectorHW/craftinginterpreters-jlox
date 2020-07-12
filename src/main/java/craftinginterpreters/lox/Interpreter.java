9889b3ace8fda8f93fcba5a9f19a631cc57f1bdc
HectorHW <hectorhw.swagman@gmail.com>
2020-07-12 18:53:15 +0300

    macros should not be closured

--- src/main/java/craftinginterpreters/lox/Interpreter.java
+++ src/main/java/craftinginterpreters/lox/Interpreter.java
@@ -361,17 +361,13 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void>{
 
     @Override
     public Void visitMacroStmt(Stmt.Macro stmt) {
-        LoxMacro macro = new LoxMacro() {
-            Environment env = environment;
-            @Override
-            public Object call(Interpreter interpreter) {
-                try{
-                    executeBlock(stmt.body, env);
-                }catch (Return r){
-                    return r.value;
-                }
-                return null;
+        LoxMacro macro = interpreter -> {
+            try{
+                executeBlock(stmt.body, environment);
+            }catch (Return r){
+                return r.value;
             }
+            return null;
         };
         environment.define(stmt.name.lexeme, macro);
         return null;
--- src/main/java/craftinginterpreters/lox/LoxMacro.java
+++ src/main/java/craftinginterpreters/lox/LoxMacro.java
@@ -3,6 +3,5 @@ package craftinginterpreters.lox;
 import java.util.List;
 
 public interface LoxMacro {
-    Environment env = null;
     Object call(Interpreter interpreter);
 }
